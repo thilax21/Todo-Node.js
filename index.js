1a976a10befb174506eceeb92e62a1fc7f098f73
thilakshana <thilakshi04@gmail.com>
2025-10-16 15:05:06 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from "cors"
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+   
+}))
+
+
 const todoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
